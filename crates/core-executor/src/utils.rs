dd3a8841cad8b46b7761396375b0841e3a3f6249
Jagdish Parihar <jatin6972@gmail.com>
2025-05-26 23:50:58 +0530

    implement conversion from NormalizedIdent to TableObject

--- crates/core-executor/src/utils.rs
+++ crates/core-executor/src/utils.rs
@@ -16,7 +16,7 @@ use datafusion::arrow::record_batch::RecordBatch;
 use datafusion::common::ScalarValue;
 use datafusion_expr::{Expr, LogicalPlan};
 use snafu::ResultExt;
-use sqlparser::ast::{Ident, ObjectName};
+use sqlparser::ast::{Ident, ObjectName, TableObject};
 use std::collections::HashMap;
 use std::sync::Arc;
 use strum::{Display, EnumString};
@@ -397,6 +397,12 @@ impl From<NormalizedIdent> for MetastoreSchemaIdent {
 impl From<NormalizedIdent> for ObjectName {
     fn from(ident: NormalizedIdent) -> Self {
         Self::from(ident.0)
+    }
+}
+
+impl From<NormalizedIdent> for TableObject {
+    fn from(ident: NormalizedIdent) -> Self {
+        TableObject::TableName(ObjectName::from(ident.0))
     }
 }
 
